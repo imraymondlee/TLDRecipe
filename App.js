8392c4c88a12036b3aca0776c73a587457860987
imraymondlee <imleeraymond@gmail.com>
2020-12-25 13:22:58 -0800

    Add page request function

--- App.js
+++ App.js
@@ -2,7 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const requestPage = function(url){
+  console.log('requestPage: ', url);
+  
+  return fetch('https://cors-anywhere.herokuapp.com/' + url)
+    .then((response) => response.text())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
 export default function App() {
+  requestPage('https://google.ca');
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
