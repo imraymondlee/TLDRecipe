f30efbdafcc7d71b96dc24b4723056b505a6edf9
imraymondlee <imleeraymond@gmail.com>
2020-12-26 16:15:14 -0800

    Extract recipe json from page

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import cheerio from 'react-native-cheerio';
 
 const requestPage = function(url) {
-  console.log('requestPage: ', url);
-
   return fetch('https://cors-anywhere.herokuapp.com/' + url)
     .then((response) => response.text())
     .then((data) => {
@@ -18,13 +16,20 @@ const requestPage = function(url) {
 }
 
 const extractStructuredData = function(data) {
-  console.log('pageData: ', data);
   const $ = cheerio.load(data);
-  return $('script[type="application/ld+json"]').text();
+  return $('script[type="application/ld+json"]').contents()[0].data;
+}
+
+const extractRecipe = function(data) {
+  let json = JSON.parse(data);
+  let recipe = json["@graph"].find(obj => {
+    return obj["@type"] === "Recipe"
+  });
+
 }
 
 export default function App() {
-  let sData = requestPage('https://www.gimmesomeoven.com/baked-chicken-breast/')
+  requestPage('https://www.gimmesomeoven.com/baked-chicken-breast/')
     .then((data) => {
       return data;
     })
@@ -32,7 +37,8 @@ export default function App() {
       return extractStructuredData(data);
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
+      extractRecipe(data);
     });
   
   return (
