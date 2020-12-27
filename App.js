b70cd005c46123f2144a326a10c58da5b301b4b9
imraymondlee <imleeraymond@gmail.com>
2020-12-26 20:16:23 -0800

    Set state for recipe information

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import cheerio from 'react-native-cheerio';
 
 const requestPage = function(url) {
@@ -20,30 +19,41 @@ const extractStructuredData = function(data) {
   return $('script[type="application/ld+json"]').contents()[0].data;
 }
 
-const extractRecipe = function(data) {
-  let json = JSON.parse(data);
-  let recipe = json["@graph"].find(obj => {
-    return obj["@type"] === "Recipe"
-  });
-
-}
-
 export default function App() {
-  requestPage('https://www.gimmesomeoven.com/baked-chicken-breast/')
-    .then((data) => {
-      return data;
-    })
-    .then((data) => {
-      return extractStructuredData(data);
-    })
-    .then((data) => {
-      // console.log(data);
-      extractRecipe(data);
-    });
+  const [recipeName, setRecipeName] = useState('Loading...');
+  const [recipeIngredients, setRecipeIngredients] = useState('Loading...');
+  const [recipeInstructions, setRecipeInstructions] = useState('Loading...');
+
+  useEffect(() => {
+    const extractRecipe = function(data) {
+      let json = JSON.parse(data);
+      let recipe = json["@graph"].find(obj => {
+        return obj["@type"] === "Recipe"
+      });
+      // debugger;
+      setRecipeName(recipe.name);
+      setRecipeIngredients(recipe.recipeIngredient);
+      setRecipeInstructions(recipe.recipeInstructions.map(r => r.text));
+    }
+ 
+    requestPage('https://www.gimmesomeoven.com/baked-chicken-breast/')
+      .then((data) => {
+        return data;
+      })
+      .then((data) => {
+        return extractStructuredData(data);
+      })
+      .then((data) => {
+        extractRecipe(data);
+      });
+
+  }, []);
   
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{recipeName}</Text>
+      <Text>{recipeIngredients}</Text>
+      <Text>{recipeInstructions}</Text>
       <StatusBar style="auto" />
     </View>
   );
